7c371a822feead2b3c24d41befa891fddd8f3002
eamv-hi <hi@M12753.ceuherning.int>
2016-03-03 08:55:38 +0100

    Version 0.1 with Users and Roles

---
+++ gs.ejbClient/ejbModule/gs/ejb/beans/UserLocal.java
@@ -0,0 +1,24 @@
+package gs.ejb.beans;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.ejb.Local;
+
+import gs.ejb.domain.User;
+
+
+@Local
+public interface UserLocal {
+
+	Optional<User> getUser(int userid);
+
+	void createUser(User user);
+
+	void updateUser(User user);
+
+	void deleteUser(User user);
+
+	List<User> listMembers();
+
+}
---
+++ gs.web/src/gs/web/UserServlet.java
@@ -0,0 +1,44 @@
+package gs.web;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import gs.ejb.beans.UserLocal;
+import gs.ejb.domain.User;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@EJB private UserLocal userejb;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<User> users = userejb.listMembers();
+//		user.setEmail("hi@eamv.dk");
+//		user.setFirstname("Hans");
+//		user.setLastname("Iversen");
+//		userejb.createUser(user);
+		response.getWriter().append("Users: " + users);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
