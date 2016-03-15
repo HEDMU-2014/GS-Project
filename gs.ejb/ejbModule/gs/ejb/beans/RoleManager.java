b4de08b3f6438518ebe2a4e6466d65b8231077e5
Karsten Heino Karlsen <k.heino.karlsen@gmail.com>
2016-03-15 13:22:10 +0100

    Define RoleManager interface

--- gs.ejb/ejbModule/gs/ejb/beans/RoleManager.java
+++ gs.ejb/ejbModule/gs/ejb/beans/RoleManager.java
@@ -1,11 +1,39 @@
 package gs.ejb.beans;
 
+import gs.ejb.domain.Role;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Stateless
 public class RoleManager implements RoleManagerLocal, RoleManagerRemote {
    @PersistenceContext
    private EntityManager em;
+
+   @Override
+   public Role findRole(int id) {
+      return null;
+   }
+
+   @Override
+   public List<Role> listRoles() {
+      return null;
+   }
+
+   @Override
+   public void createRole(Role role) {
+
+   }
+
+   @Override
+   public void updateRole(Role role) {
+
+   }
+
+   @Override
+   public void deleteRole(Role role) {
+
+   }
 }
--- gs.ejbClient/ejbModule/gs/ejb/beans/RoleManagerCommon.java
+++ gs.ejbClient/ejbModule/gs/ejb/beans/RoleManagerCommon.java
@@ -1,4 +1,17 @@
 package gs.ejb.beans;
 
+import gs.ejb.domain.Role;
+
+import java.util.List;
+
 public interface RoleManagerCommon {
+   Role findRole(int id);
+
+   List<Role> listRoles();
+
+   void createRole(Role role);
+
+   void updateRole(Role role);
+
+   void deleteRole(Role role);
 }
