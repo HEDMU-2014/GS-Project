27b9c5af69fc7930d7e546e1bfc9a38863393e04
Karsten Heino Karlsen <k.heino.karlsen@gmail.com>
2016-03-17 09:26:59 +0100

    Add missing properties in Picture

--- gs.ejbClient/ejbModule/gs/ejb/domain/Picture.java
+++ gs.ejbClient/ejbModule/gs/ejb/domain/Picture.java
@@ -1,9 +1,16 @@
 package gs.ejb.domain;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Picture implements Serializable {
    private long id;
+
+   private User user;
+
+   private String imageUrl;
+
+   private Date creationDate;
 
    public long getId() {
       return id;
@@ -11,5 +18,29 @@ public class Picture implements Serializable {
 
    public void setId(long id) {
       this.id = id;
+   }
+
+   public User getUser() {
+      return user;
+   }
+
+   public void setUser(User user) {
+      this.user = user;
+   }
+
+   public String getImageUrl() {
+      return imageUrl;
+   }
+
+   public void setImageUrl(String imageUrl) {
+      this.imageUrl = imageUrl;
+   }
+
+   public Date getCreationDate() {
+      return creationDate;
+   }
+
+   public void setCreationDate(Date creationDate) {
+      this.creationDate = creationDate;
    }
 }
