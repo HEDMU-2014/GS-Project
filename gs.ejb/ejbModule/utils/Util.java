312b7fb14f9168dabfcb90044d1090fc5504d89f
Hans Iversen <hi@eamv.dk>
2016-04-02 09:32:44 +0200

    commented lines removed

--- gs.ejb/ejbModule/utils/Util.java
+++ gs.ejb/ejbModule/utils/Util.java
@@ -45,11 +45,5 @@ public class Util {
 		MessageDigest md = MessageDigest.getInstance("SHA-256");
 		byte[] mdbytes = md.digest(txt.getBytes());
 		return Base64.getEncoder().encodeToString(mdbytes);
-		//convert the byte to hex format method 1
-//        StringBuffer sb = new StringBuffer();
-//        for (int i = 0; i < mdbytes.length; i++) {
-//          sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
-//        }
-//		return sb.toString();
 	}
 }
