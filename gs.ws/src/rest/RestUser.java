3a5dc5e7885e43ae69bf4066bed8ca9eb9f0567b
Hans Iversen <hi@eamv.dk>
2016-04-04 20:01:13 +0200

    Rest-service for Users changed. In User, LocalDateTime changed to Calendar, due to JAX-RS version in Wildfly

--- gs.ws/src/rest/RestUser.java
+++ gs.ws/src/rest/RestUser.java
@@ -1,9 +1,14 @@
 package rest;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -26,6 +31,73 @@ public class RestUser {
 			user = opt.get();
 		}
 		return user;
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/readid/{key}")
+	public User getUserById(@PathParam("key") long key) {
+		User user = null;
+		Optional<User> opt = ejb.read(key);
+		if (opt.isPresent()) {
+			user = opt.get();
+		}
+		return user;
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/read/{email}")
+	public User getUserByEmail(@PathParam("email") String email) {
+		User user = null;
+		Optional<User> opt = ejb.read(email);
+		if (opt.isPresent()) {
+			user = opt.get();
+		}
+		return user;
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/search/{search}")
+	public List<User> searchUsers(@PathParam("search") String search) {
+		return ejb.searchUsers(search);
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/members/{organization}")
+	public List<User> listMembers(@PathParam("organization") String organization) {
+		return ejb.listMembers(organization);
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("")
+	public boolean create(User user) {
+		ejb.create(user);
+		return true;
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("")
+	public boolean update(User user) {
+		ejb.update(user);
+		return true;
+	}
+	
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("delete/{key}")
+	public boolean delete(@PathParam("key") long key) {
+		User user = new User();
+		user.setUserid(key);
+		ejb.delete(user);
+		return true;
 	}
 
 }
