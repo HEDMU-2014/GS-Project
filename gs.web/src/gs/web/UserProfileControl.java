5b10f588d99eca8ad2b3e3b9f4332240b2d7433b
bjorn <bjorngit@bjeroken.com>
2016-04-26 13:18:08 +0200

    Select Many checkbox attempt for roles

--- gs.web/src/gs/web/UserProfileControl.java
+++ gs.web/src/gs/web/UserProfileControl.java
@@ -1,6 +1,9 @@
 package gs.web;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -12,33 +15,65 @@ import javax.inject.Named;
 
 import org.jboss.logging.Logger;
 
+import beans.OrganizationBeanLocal;
+import beans.PictureBeanLocal;
+import beans.RolesBeanLocal;
 import beans.UserProfilesBeanLocal;
 import beans.UsersBeanLocal;
+import domain.Role;
 
 @Named
 @SessionScoped
 public class UserProfileControl implements Serializable {
-	
-	@Inject UserProfileModel model;
+
+	@Inject
+	UserProfileModel model;
 
 	private static final long serialVersionUID = 1L;
 
-	@EJB private UsersBeanLocal ubl;
-	@EJB private UserProfilesBeanLocal upbl;
+	@EJB
+	private UsersBeanLocal ubl;
+	@EJB
+	private UserProfilesBeanLocal upbl;
+	@EJB
+	private RolesBeanLocal rbl;
+	@EJB
+	private OrganizationBeanLocal obl;
+	@EJB
+	private PictureBeanLocal pbl;
+
 	private Logger logger = Logger.getLogger(UserProfileControl.class);
-	
+
 	@PostConstruct
 	public void init() {
+		Role[] selectedRoles = new Role[model.getUser().getRoles().size()];
+		Iterator<Role> iter = model.getUser().getRoles().iterator();
+		int i = 0;
+		while (iter.hasNext()) {
+			selectedRoles[i] = iter.next();
+			i++;
+		}
+		model.setSelectedRoles(selectedRoles);
 	}
+
 	public void update() {
 		logger.info("method update entered");
 		try {
+			model.getUser().setRoles(Arrays.asList(model.getSelectedRoles()));
 			ubl.update(model.getUser());
 			upbl.update(model.getUserProfile());
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("User + UserProfile updated"));
 		} catch (Exception e) {
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, e.toString(), e.getMessage()));
+			System.out.println(e.toString());
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, e.toString(), e.getMessage()));
+
 		}
 	}
-	
+
+	public List<Role> getRoles() {
+		logger.info("method update entered");
+		return rbl.allRoles();
+	}
+
 }
--- gs.web/src/gs/web/UserProfileModel.java
+++ gs.web/src/gs/web/UserProfileModel.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
+import domain.Organization;
+import domain.Picture;
+import domain.Role;
 import domain.User;
 import domain.UserProfile;
 
@@ -14,7 +17,11 @@ public class UserProfileModel implements Serializable {
 	
 	private User user;
 	private UserProfile userProfile;
+	private Role role;
+	private Organization org;
+	private Picture picture;
 	private boolean edit = false;
+	private Role[] selectedRoles;
 
 	private static final long serialVersionUID = 1L;
 
@@ -48,4 +55,29 @@ public class UserProfileModel implements Serializable {
 	public boolean isShowUpdate() {
 		return edit && user.getUserid() > 0;
 	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	public Organization getOrg() {
+		return org;
+	}
+	public void setOrg(Organization org) {
+		this.org = org;
+	}
+	public Picture getPicture() {
+		return picture;
+	}
+	public void setPicture(Picture picture) {
+		this.picture = picture;
+	}
+	public Role[] getSelectedRoles() {
+		return selectedRoles;
+	}
+	public void setSelectedRoles(Role[] selectedRoles) {
+		this.selectedRoles = selectedRoles;
+	}
+	
 }
